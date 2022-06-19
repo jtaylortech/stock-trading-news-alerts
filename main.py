530292e6c61e7171ef93331118d4ee6537f1780f
Jarred Taylor <jarrede20@gmail.com>
2022-06-18 23:37:24 -0400

    established contact with alpha vantage api and printed test data in json format (fixed some syntax)

--- main.py
+++ main.py
@@ -5,17 +5,17 @@ STOCK_NAME = "TSLA"
 COMPANY_NAME = "Tesla Inc"
 
 STOCK_ENDPOINT = "https://www.alphavantage.co/query"
-stock_api_key = jt_stock_api
+STOCK_API_KEY = jt_stock_api
 
 # getting yesterdays closing stock price
 stock_params = {
     "function": "TIME_SERIES_DAILY",
     "symbol": STOCK_NAME,
-    "apikey":stock_api_key
+    "apikey": STOCK_API_KEY,
 }
 
 response = requests.get(STOCK_ENDPOINT, params=stock_params)
-print(response.json)
+print(response.json())
 
 
 
